84a34e6f30997d5405c8dd145eac6d113d089895
mentorque <mentorque.edu@gmail.com>
2025-08-03 03:56:24 +0100

    Update Navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -68,16 +68,16 @@ const Navigation = () => {
                 <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
               </svg>
             </a>
-            <a
+{/*             <a
               href="tel:+353894145256"
               className="text-muted-foreground hover:text-primary transition-colors"
             >
               <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
                 <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
               </svg>
-            </a>
+            </a> */}
             <a
-              href="#"
+              href="https://www.linkedin.com/in/komal-joshi-03353320a/"
               className="text-muted-foreground hover:text-primary transition-colors"
             >
               <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
