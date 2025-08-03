f6bca3551f33aaf4600e012040477112f7da1da1
mentorque <mentorque.edu@gmail.com>
2025-08-03 03:51:08 +0100

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -20,7 +20,6 @@ const Footer = () => {
             </a>
             <a 
               href="https://www.linkedin.com/in/komal-joshi-03353320a/"
-              target="_blank"
               rel="noopener noreferrer"
               className="text-gray-400 hover:text-blue-400 transition-colors"
               aria-label="LinkedIn"
